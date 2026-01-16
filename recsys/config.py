9d748f0b70becd0ce5d2be9d8ca840dbf68811d6
Vadim Sokoltsov <vforvad@gmail.com>
2026-01-16 22:45:13 +0100

    Add config file

---
+++ recsys/config.py
@@ -0,0 +1,12 @@
+import os
+from pathlib import Path
+
+ROOT = Path(
+    os.path.abspath(
+        os.path.join(os.path.dirname(__file__), "..")
+    )
+)
+DATA = os.path.join(ROOT, "data")
+RAW_DATA = os.path.join(DATA, "raw")
+MOVIELENS_PATH = os.path.join(RAW_DATA, "ml-1m")
+MODELS = os.path.join(DATA, "models")
