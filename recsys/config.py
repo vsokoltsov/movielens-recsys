fa03257bde324b3d97752c2f5be7565daf7663ed
Vadim Sokoltsov <vforvad@gmail.com>
2026-01-18 16:47:04 +0100

    Fix linter in config.py

--- recsys/config.py
+++ recsys/config.py
@@ -1,11 +1,7 @@
 import os
 from pathlib import Path
 
-ROOT = Path(
-    os.path.abspath(
-        os.path.join(os.path.dirname(__file__), "..")
-    )
-)
+ROOT = Path(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 DATA = os.path.join(ROOT, "data")
 RAW_DATA = os.path.join(DATA, "raw")
 MOVIELENS_PATH = os.path.join(RAW_DATA, "ml-1m")
