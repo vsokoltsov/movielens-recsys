94bd3d2e095ff2a7598c87c95470e079610bea97
Vadim Sokoltsov <vforvad@gmail.com>
2026-01-19 16:55:32 +0100

    Implement loading of files from cloud instead of local machine

--- recsys/api/app.py
+++ recsys/api/app.py
@@ -4,16 +4,16 @@ import os
 from typing import List, Any, cast, AsyncGenerator
 from contextlib import asynccontextmanager
 
-import numpy as np
-import pandas as pd
 from fastapi import FastAPI, Depends, HTTPException, Path, Query
 from pydantic import BaseModel
 
 from recsys.aggregates import ModelType, Movie
-from recsys.utils import read_from_csv
+from recsys.utils import read_from_csv, read_from_bytes
 from recsys.config import MOVIELENS_PATH, MODELS
 from recsys.api.config import get_settings
 from recsys.recommender import Recommender
+from recsys.gcp import GCPStorageClient
+from dotenv import load_dotenv
 
 
 class RecsResponse(BaseModel):
@@ -21,25 +21,69 @@ class RecsResponse(BaseModel):
 
 @asynccontextmanager
 async def lifespan(app: FastAPI) -> AsyncGenerator[Any, None]:
+    load_dotenv()
+
     settings = get_settings()
-    app.state.movies = read_from_csv(
-        path=os.path.join(MOVIELENS_PATH, "movies.dat"),
-        columns=["movie_id", "title", "genres"],
-    )
-    app.state.ratings = read_from_csv(
-        path=os.path.join(MOVIELENS_PATH, "ratings.dat"),
-        columns=["user_id", "movie_id", "rating", "timestamp"],
-    )
-    app.state.users = read_from_csv(
-        path=os.path.join(MOVIELENS_PATH, "users.dat"),
-        columns=["user_id", "gender", "age", "occupation", "zip"],
-    )
+    model_path = os.path.join(MODELS, settings.MODEL_NAME)
+    gcs_client = GCPStorageClient()
+    model_bucket = os.environ.get('MODEL_BUCKET')
+    raw_bucket = os.environ.get('RAW_BUCKET')
+    movies_path = os.path.join(MOVIELENS_PATH, "movies.dat")
+    
+    if model_bucket:
+        model_dest_path = os.path.join("/tmp", settings.MODEL_NAME)
+        gcs_client.download(
+            bucket_name=str(model_bucket),
+            object_name=settings.MODEL_NAME,
+            dst_path=model_dest_path
+        )
+        model_path = model_dest_path
+
+    if raw_bucket:
+        users_df = read_from_bytes(
+            bts=gcs_client.read_bytes(
+                bucket=raw_bucket, 
+                obj="ml-1m/users.dat"
+            ),
+            columns=["user_id", "gender", "age", "occupation", "zip"]
+        )
+        movies_df = read_from_bytes(
+            bts=gcs_client.read_bytes(
+                bucket=raw_bucket, 
+                obj="ml-1m/movies.dat"
+            ),
+            columns=["movie_id", "title", "genres"]
+        )
+        ratings_df = read_from_bytes(
+            bts=gcs_client.read_bytes(
+                bucket=raw_bucket, 
+                obj="ml-1m/ratings.dat"
+            ),
+            columns=["user_id", "movie_id", "rating", "timestamp"]
+        )
+    else:
+        users_df = read_from_csv(
+            path=os.path.join(MOVIELENS_PATH, "users.dat"),
+            columns=["user_id", "gender", "age", "occupation", "zip"],
+        )
+        movies_df = read_from_csv(
+            path=movies_path,
+            columns=["movie_id", "title", "genres"],
+        )
+        ratings_df = read_from_csv(
+            path=os.path.join(MOVIELENS_PATH, "ratings.dat"),
+            columns=["user_id", "movie_id", "rating", "timestamp"],
+        )
+    
+    app.state.movies = movies_df
+    app.state.ratings = ratings_df
+    app.state.users = users_df
     app.state.recommender = Recommender(
         model_type=settings.MODEL_TYPE,
         source=settings.SOURCE,
         movies=app.state.movies,
         rating_threshold=settings.RATING_THRESHOLD,
-        model_path=os.path.join(MODELS, settings.MODEL_NAME)
+        model_path=model_path
     )
     yield
 
@@ -65,7 +109,6 @@ def get_recommendations(
         movies: List[Movie] = recommender.recommend(user_id=id, n_items=k)
         return RecsResponse(movies=movies)
     except Exception as e:
-        # e.g., neural_mf not implemented
         raise HTTPException(status_code=501, detail=e)
 
 if __name__ == "__main__":
