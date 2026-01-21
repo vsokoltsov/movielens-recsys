d3a9c572db401593c98da0a32c2f6af5656d59d2
Vadim Sokoltsov <vforvad@gmail.com>
2026-01-21 12:07:52 +0100

    Fix api config file

--- recsys/api/config.py
+++ recsys/api/config.py
@@ -1,14 +1,27 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from recsys.aggregates import ModelType, Source
 from functools import lru_cache
+from typing import Optional
 
 class Settings(BaseSettings):
     MODEL_TYPE: ModelType
     MODEL_NAME: str
     SOURCE: Source
     RATING_THRESHOLD: int
-    USE_GCP: bool
-    GOOGLE_APPLICATION_CREDENTIALS: str
+    RAW_BUCKET: Optional[str]
+    MODEL_BUCKET: Optional[str]
+    DB_NAME: str
+    DB_HOST: str
+    DB_PORT: int
+    DB_USER: str
+    DB_PASSWORD: str
+
+    @property
+    def DATABASE_URL(self) -> str:
+        return (
+            f"postgresql+psycopg://{self.DB_USER}:{self.DB_PASSWORD}"
+            f"@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+        )
 
     # model_config = SettingsConfigDict(env_file=".env")
 
