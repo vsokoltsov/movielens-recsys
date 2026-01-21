ab79d453f9b297664e948be1be6060aa6f38f817
Vadim Sokoltsov <vforvad@gmail.com>
2026-01-21 12:09:31 +0100

    Fix train script accordingly

--- recsys/modeling/train.py
+++ recsys/modeling/train.py
@@ -1,3 +1,4 @@
+import asyncio
 import os
 from typing import Optional
 
@@ -5,12 +6,44 @@ import click
 from dotenv import load_dotenv
 
 from recsys.config import MOVIELENS_PATH, MODELS
-from recsys.modeling.training.item_knn import train_item_knn
 from recsys.modeling.als import AlternatingLeastSquaresRecommender
 from recsys.modeling.item_knn import ItemKNNRecommender
 from recsys.modeling.dataset import Dataset
-from recsys.gcp import GCPStorageClient
+from recsys.gcp import GCPModelStorage
+from recsys.db.session import AsyncSessionLocal
+from recsys.db.repositories.ratings import RatingsRepository
 
+async def train_model(model_type: str, bucket_name: Optional[str]):
+    load_dotenv()
+
+    env_bucket = os.environ.get('MODEL_BUCKET', bucket_name)
+    dataset = Dataset(dataset_path=MOVIELENS_PATH)
+    async with AsyncSessionLocal() as session:
+        ratings_repo = RatingsRepository(session)
+        model_storage = GCPModelStorage(bucket_name=str(env_bucket))
+        if model_type == "als":
+            click.echo("Train alternating least squares model...")
+            als = AlternatingLeastSquaresRecommender(
+                ratings_repo=ratings_repo,
+                storage=model_storage,
+                threshold=4,
+                model_path="als/latest/model.npz",
+                x_ui_path="als/latest/x_ui.npz",
+                mappings_path="als/latest/mappings.json",
+            )
+            await als.fit(dataset.ratings)
+            await als.save()
+        elif model_type == "item_knn":
+            click.echo("Train item knn model...")
+            knn = ItemKNNRecommender(
+                ratings_repo=ratings_repo,
+                storage=model_storage,
+                artifact_prefix="item_knn/v1",
+                k_neighbors=200, 
+                threshold=4
+            )
+            await knn.fit()
+            await knn.save()
 
 @click.command()
 @click.option(
@@ -22,40 +55,8 @@ from recsys.gcp import GCPStorageClient
     "--bucket-name",
     help='Use of Google Cloud Storage',
 )
-
-def train_model(model_type: str, bucket_name: Optional[str]):
-    load_dotenv()
-
-    env_bucket = os.environ.get('MODEL_BUCKET', bucket_name)
-    gcs_client = GCPStorageClient()
-    dataset = Dataset(dataset_path=MOVIELENS_PATH)
-    if model_type == "als":
-        model_name = "alternating_least_squares.npz"
-        model_path = os.path.join(MODELS, "alternating_least_squares.npz")
-        click.echo("Train alternating least squares model...")
-        als = AlternatingLeastSquaresRecommender(
-            threshold=4,
-            model_path=model_path
-        )
-        als.fit(dataset.ratings)
-        als.save()
-        
-        
-    elif model_type == "item_knn":
-        model_name = "item_knn_recommender.pkl"
-        model_path = os.path.join("/tmp", model_name)
-        click.echo("Train item knn model...")
-        knn = ItemKNNRecommender(k_neighbors=200, threshold=4)
-        knn.fit(dataset.ratings)
-        knn.save(model_path)
-
-    if env_bucket is not None:
-            gcs_client.upload(
-                path=model_path,
-                bucket_name=env_bucket,
-                object_name=model_name
-            )
-
+def main(model_type: str, bucket_name: str) -> None:
+    asyncio.run(train_model(model_type, bucket_name))
 
 if __name__ == "__main__":
-    train_model()
+    main()
