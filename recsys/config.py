296b4935f79f58a5e2587ad687174d48b034ab63
Vadim Sokoltsov <vforvad@gmail.com>
2026-01-21 12:04:29 +0100

    Update common config file

--- recsys/config.py
+++ recsys/config.py
@@ -1,8 +1,15 @@
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+from recsys.db.session import DATABASE_URL
+load_dotenv()
 
 ROOT = Path(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 DATA = os.path.join(ROOT, "data")
 RAW_DATA = os.path.join(DATA, "raw")
 MOVIELENS_PATH = os.path.join(RAW_DATA, "ml-1m")
 MODELS = os.path.join(DATA, "models")
+RAW_BUCKET = os.environ.get("RAW_BUCKET")
+MODEL_BUCKET = os.environ.get("MODEL_BUCKET")
+DATABASE_URL = os.environ.get("DATABASE_URL", "postgresql+psycopg://user:pass@postgres:5432/recsys")
