4b3725f34bbc96f9d9ef659d1b2fd5a07602d0d6
Vadim Sokoltsov <vforvad@gmail.com>
2026-01-29 01:10:39 +0100

    Move session dependency to to api package

--- recsys/api/dependencies.py
+++ recsys/api/dependencies.py
@@ -4,9 +4,25 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from recsys.db.repositories.ratings import RatingsRepository
 from recsys.db.repositories.movies import MoviesRepository
 from recsys.context import RequestContext, set_request_ctx
-from recsys.db.session import get_db_session
 from fastapi import Request
+from typing import cast
+from collections.abc import AsyncGenerator
+from sqlalchemy import exc
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    async_sessionmaker,
+)
 
+async def get_db_session(request: Request) -> AsyncGenerator[AsyncSession, None]:
+    sessionmaker = cast(async_sessionmaker[AsyncSession], request.app.state.sessionmaker)
+    async with sessionmaker() as session:
+        try:
+            yield session
+        except exc.SQLAlchemyError:
+            await session.rollback()
+            raise
+        finally:
+            await session.close()
 
 async def init_request_context(
     session: AsyncSession = Depends(get_db_session),
