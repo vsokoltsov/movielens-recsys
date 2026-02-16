c0f5e8519c8f1d95ab5a28bc53afd9ca08230c4d
Vadim Sokoltsov <vforvad@gmail.com>
2026-02-16 21:04:40 +0100

    Change train cli script for support both csv and db

--- recsys/modeling/train.py
+++ recsys/modeling/train.py
@@ -1,67 +1,99 @@
 import asyncio
-import os
+import pandas as pd
 from typing import Optional
 
 import click
-from dotenv import load_dotenv
 
-from recsys.config import MOVIELENS_PATH, DATABASE_URL
+from recsys.config import get_settings, Source, Setup
 from recsys.aggregates import ModelType
 from recsys.modeling.als import AlternatingLeastSquaresRecommender
 from recsys.modeling.item_knn import ItemKNNRecommender
 from recsys.modeling.torch import PytorchRecommender
 from recsys.modeling.dataset import Dataset
-from recsys.gcp import GCPModelStorage
+from recsys.storage import GCPModelStorage, LocalStorage, StorageProtocol
 from recsys.db.session import build_sessionmaker, session_scope
-from recsys.db.repositories.ratings import RatingsRepository
+from recsys.repository.protocols import RatingsRepositoryProtocol
+from recsys.repository.db.ratings import RatingsDBRepository
+from recsys.repository.csv.ratings import RatingsCSVRepository
+
+
+async def _run_training(
+    model_type: ModelType,
+    ratings_df: pd.DataFrame,
+    ratings_repo: RatingsRepositoryProtocol,
+    storage: StorageProtocol,
+):
+    if model_type == ModelType.ALS:
+        click.echo("Train alternating least squares model...")
+        als = AlternatingLeastSquaresRecommender(
+            ratings_repo=ratings_repo,
+            storage=storage,
+            threshold=4,
+            model_path="als/latest/model.npz",
+            x_ui_path="als/latest/x_ui.npz",
+            mappings_path="als/latest/mappings.json",
+        )
+        await als.fit(ratings_df)
+        await als.save()
+    elif model_type == ModelType.ITEM_KNN:
+        click.echo("Train item knn model...")
+        knn = ItemKNNRecommender(
+            ratings_repo=ratings_repo,
+            storage=storage,
+            artifact_prefix="item_knn/v1",
+            k_neighbors=200,
+            threshold=4,
+        )
+        await knn.fit()
+        await knn.save()
+    elif model_type == ModelType.PYTORCH:
+        click.echo("Train pytorch model...")
+        ptr = PytorchRecommender(
+            storage=storage,
+            model_path="pytorch/latest/model.pt",
+            x_ui_path="pytorch/latest/x_ui.npz",
+            mappings_path="pytorch/latest/mappings.json",
+            ratings_repo=ratings_repo,
+        )
+        await ptr.fit(ratings_df)
+        await ptr.save()
 
 
 async def train_model(model_type: str, bucket_name: Optional[str]):
-    load_dotenv()
+    settings = get_settings()
+    storage: StorageProtocol
+    ratings_repo: RatingsRepositoryProtocol
 
-    env_bucket = os.environ.get("MODEL_BUCKET", bucket_name)
-    dataset = Dataset(dataset_path=MOVIELENS_PATH)
-    engine, session_local = build_sessionmaker(database_url=DATABASE_URL)
+    if settings.SETUP == Setup.LOCAL:
+        storage = LocalStorage(model_path=settings.MODELS_PATH)
+    elif settings.SETUP == Setup.CLOUD:
+        storage = GCPModelStorage(bucket_name=bucket_name or settings.MODEL_BUCKET)
 
-    async with session_scope(session_local) as session:
-        ratings_repo = RatingsRepository(session)
-        model_storage = GCPModelStorage(bucket_name=str(env_bucket))
-        if model_type == ModelType.ALS:
-            click.echo("Train alternating least squares model...")
-            als = AlternatingLeastSquaresRecommender(
-                ratings_repo=ratings_repo,
-                storage=model_storage,
-                threshold=4,
-                model_path="als/latest/model.npz",
-                x_ui_path="als/latest/x_ui.npz",
-                mappings_path="als/latest/mappings.json",
-            )
-            await als.fit(dataset.ratings)
-            await als.save()
-        elif model_type == ModelType.ITEM_KNN:
-            click.echo("Train item knn model...")
-            knn = ItemKNNRecommender(
-                ratings_repo=ratings_repo,
-                storage=model_storage,
-                artifact_prefix="item_knn/v1",
-                k_neighbors=200,
-                threshold=4,
-            )
-            await knn.fit()
-            await knn.save()
-        elif model_type == ModelType.PYTORCH:
-            click.echo("Train pytorch model...")
-            ptr = PytorchRecommender(
-                storage=model_storage,
-                model_path="pytorch/latest/model.pt",
-                x_ui_path="pytorch/latest/x_ui.npz",
-                mappings_path="pytorch/latest/mappings.json",
-                ratings_repo=ratings_repo,
-            )
-            await ptr.fit(dataset.ratings)
-            await ptr.save()
-
-        await engine.dispose()
+    if settings.SOURCE == Source.DB:
+        engine, session_local = build_sessionmaker(database_url=settings.DATABASE_URL)
+        try:
+            async with session_scope(session_local) as session:
+                ratings_repo = RatingsDBRepository(session=session)
+                ratings_df = await ratings_repo.fetch_ratings_df(min_rating=0)
+                await _run_training(
+                    model_type=ModelType(model_type),
+                    ratings_df=ratings_df,
+                    ratings_repo=ratings_repo,
+                    storage=storage,
+                )
+        finally:
+            await engine.dispose()
+    elif settings.SOURCE == Source.CSV:
+        dataset = Dataset(dataset_path=settings.MOVIELENS_PATH)
+        ratings_repo = RatingsCSVRepository(df=dataset.ratings)
+        await _run_training(
+            model_type=ModelType(model_type),
+            ratings_df=dataset.ratings,
+            ratings_repo=ratings_repo,
+            storage=storage,
+        )
+    else:
+        raise ValueError("Invalid source")
 
 
 @click.command()
